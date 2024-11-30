ead62c278d16081fe327c765183338b1ef3a46af
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-11-30 18:53:10 -0400

    Cleaning up

--- Rogers Toolbox v3.0/MainWindow.xaml.cs
+++ Rogers Toolbox v3.0/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using System.Text;
 
 namespace Rogers_Toolbox_v3._0
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
 
@@ -126,7 +123,9 @@ namespace Rogers_Toolbox_v3._0
                 InfoBox.Content = ($"{remainingSerials} Serials Loaded");
             }
         }
+        
         // For Importing Serials from Excel
+
         private void OpenExcel()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
