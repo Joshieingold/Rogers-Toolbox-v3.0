6c38e616d365c32f5485d1484a3dd9c2ced1f8fc
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-12-21 16:13:04 -0400

    ctr update  is now fully customizable in settings

--- Rogers Toolbox v3.0/MainWindow.xaml.cs
+++ Rogers Toolbox v3.0/MainWindow.xaml.cs
@@ -44,7 +44,12 @@ namespace Rogers_Toolbox_v3._0
         private static List<string> failedList = new List<string>(); // For WMS import storing the failed serials.
         private static int ctrImportSpeed = 0; // The speed that the user will get to click input locations between CTRS.
         private int remainingSerials; // Stores the count of remaining serials.
-        
+
+        private string CtrString = null;
+        private string RobString = null;
+        private bool CombineCTR = true;
+
+
 
         // Base Functions
 
@@ -67,6 +72,10 @@ namespace Rogers_Toolbox_v3._0
             wmsCheckPixel = Properties.Settings.Default.WMSCheckPixel;
             ctrImportSpeed = Properties.Settings.Default.CTRUpdateSpeed;
             isBomWip = Properties.Settings.Default.IsBomWip;
+
+            CtrString = Properties.Settings.Default.CTRString;
+            RobString = Properties.Settings.Default.RobitailleString;
+            CombineCTR = Properties.Settings.Default.CombinedCTRsBool;
 
 
 
@@ -546,13 +555,25 @@ namespace Rogers_Toolbox_v3._0
             }
         } // Uses a dictionary to get data totals.
 
-        private List<string> ConvertStringstoCTR(string SettingsList)
+        private List<string> ConvertStringsToCTR(string SettingsList)
         {
-
+            // Split the input string into an array of strings
             string[] items = SettingsList.Split(' ');
 
             // Create a List to maintain order and uniqueness
             List<string> orderedUniqueList = new List<string>();
+
+            // Iterate over each item
+            foreach (string item in items)
+            {
+                // Add the item to the list only if it is not already present
+                if (!orderedUniqueList.Contains(item))
+                {
+                    orderedUniqueList.Add(item);
+                }
+            }
+
+            // Return the unique and ordered list
             return orderedUniqueList;
         }
         private List<string> AnalyzeSheet(IXLWorksheet sheet) // loops through each contractor (many times) and stores the data for them given their list of devices.
@@ -562,8 +583,8 @@ namespace Rogers_Toolbox_v3._0
 
 
 
-            var CTRList = new HashSet<string> { "8052", "8067", "8975", "8986", "8990", "8994", "8997" };
-            var robitailleList = new HashSet<string> { "8017", "8037", "8038", "8041", "8047", "8080", "8093" };
+            var CTRList = ConvertStringsToCTR(CtrString);
+            var robitailleList = ConvertStringsToCTR(RobString);
             var combinedCTRS = new List<string> { "8993", "8982" };
             var warehouseList = new HashSet<string> { "NB1", "NF1" };
 
@@ -593,6 +614,26 @@ namespace Rogers_Toolbox_v3._0
             {
                 "XB8", "XB7", "XI6", "XIONE", "PODS", "ONTS","CAM1", "CAM2", "CAM3", "ENTOS", "CODA"
             };
+
+            allContractors = new List<string>();
+            foreach (string robctr in robitailleList )
+            {
+                allContractors.Add(robctr);
+            }
+            foreach (string ctr in CTRList)
+            {
+                allContractors.Add(ctr);
+            }
+            if (CombineCTR) 
+            {
+                allContractors.Add("8993 & 8982");
+            }
+            foreach (string house in warehouseList)
+            {
+                allContractors.Add(house);
+            }
+            allContractors.Add("Cleaning Up");
+
 
 
             // Processing robitaille
@@ -633,20 +674,24 @@ namespace Rogers_Toolbox_v3._0
             // Processing Combined CTRS
 
             // Placing this outside in the hope that it will combine the numbers oganically
-            var combinedContractorTotals = contractorDevices.ToDictionary(device => device, device => 0);
-            foreach (var row in sheet.RowsUsed().Skip(2))
+            if (CombineCTR)
             {
-                var contractorId = row.Cell(8).GetValue<string>().Trim(); // Column H
-                var itemCode = row.Cell(6).GetValue<string>();           // Column F
-                var inventoryType = row.Cell(10).GetValue<string>();      // Column J
-
-                if ((contractorId == "8993" || contractorId == "8982") && inventoryType.StartsWith("CTR.Subready."))
+                var combinedContractorTotals = contractorDevices.ToDictionary(device => device, device => 0);
+                foreach (var row in sheet.RowsUsed().Skip(2))
                 {
-                    UpdateTotals(combinedContractorTotals, itemCode, contractorDevices, deviceMapping);
+                    var contractorId = row.Cell(8).GetValue<string>().Trim(); // Column H
+                    var itemCode = row.Cell(6).GetValue<string>();           // Column F
+                    var inventoryType = row.Cell(10).GetValue<string>();      // Column J
+
+                    if ((contractorId == "8993" || contractorId == "8982") && inventoryType.StartsWith("CTR.Subready."))
+                    {
+                        UpdateTotals(combinedContractorTotals, itemCode, contractorDevices, deviceMapping);
+                    }
                 }
+                var formattedTotals = FormatTotals(combinedContractorTotals, contractorDevices);
+                results.Add(formattedTotals);
             }
-            var formattedTotals = FormatTotals(combinedContractorTotals, contractorDevices);
-            results.Add(formattedTotals);
+            
 
 
             // Processing Warehouses.
--- Rogers Toolbox v3.0/Properties/Settings.Designer.cs
+++ Rogers Toolbox v3.0/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Rogers_Toolbox_v3._0.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -176,18 +176,6 @@ namespace Rogers_Toolbox_v3._0.Properties {
             }
             set {
                 this["CTRString"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Setting {
-            get {
-                return ((string)(this["Setting"]));
-            }
-            set {
-                this["Setting"] = value;
             }
         }
         
