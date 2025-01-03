a0a608cf8d1860bfde89096b00a6b29a8a271c15
Joshua Lynch <149664064+Joshieingold@users.noreply.github.com>
2025-01-03 14:19:12 -0400

    Update MainWindow.xaml.cs

--- Rogers Toolbox v3.0/MainWindow.xaml.cs
+++ Rogers Toolbox v3.0/MainWindow.xaml.cs
@@ -1199,6 +1199,7 @@ namespace Rogers_Toolbox_v3._0
 // 6.1. Updated remotely, the goal could be compared to data we have and show percent of completion.
 // 6.2. Could also show the amount of devices needed per day to maintain on track. updated based on the latest data! 
 // 7. Add hover labels for the buttons.
+// 8. Maybe make the database styling nicer.
 
 // 3.1 Change Log:
 // x. Make printing things use the textbox not the serial list. 
